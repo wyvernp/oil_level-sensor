8df210bb27b28fe5a0ac67de52db85c8729685e0
wyvernp <35661956+wyvernp@users.noreply.github.com>
2022-01-19 16:01:06 +0000

    Stupid esp timers

--- oil_level_sensor.ino
+++ oil_level_sensor.ino
@@ -22,12 +22,8 @@
 
 
 HCSR04 hc(13, 12); // Initialize Pin D7, D6
-
-String serverName = "http://192.168.1.xxx:xxx/oil-level";
-
-void take_reading() {
-
-}
+int clockUpdateCounter = 1440;
+String serverName = "http://192.168.1.xxx/oil-level";
 
 void setup() {
   // put your setup code here, to run once:
@@ -44,33 +40,45 @@ void setup() {
 
   Serial.print("Connected, IP address: ");
   Serial.println(WiFi.localIP());
-  String distance_reading = "0.00";
-  while (distance_reading == "0.00") {
-    // put your main code here, to run repeatedly:
-    distance_reading = String(hc.dist());
-    //Serial.println(hc.dist()); // Print in centimeters the value from the sensor
-    if(WiFi.status()== WL_CONNECTED){
-        if(distance_reading!= "0.00") {
-            WiFiClient client;
-            HTTPClient http;
-            http.begin(client, serverName);
-            http.addHeader("Content-Type", "application/json");
-            String jsonString = "{\"level\":\"" + distance_reading + "\"}";
-            int httpResponseCode = http.POST(jsonString);
-            Serial.print("HTTP Response code: ");
-            Serial.println(httpResponseCode);
-            Serial.print(distance_reading);
-            // Free resources
-            http.end();
-        } 
 
-      }
-  }
-  // Sleep
-  Serial.println("ESP8266 in sleep mode");
-  ESP.deepSleep(8.64e10);
 }
 
 void loop() {
-
+  // put your main code here, to run repeatedly:
+  int delaytime = 10000;
+  if (clockUpdateCounter == 1440) {
+  String distance_reading = "0.00";
+    while (distance_reading == "0.00") {
+      // put your main code here, to run repeatedly:
+      distance_reading = String(hc.dist());
+      //Serial.println(hc.dist()); // Print in centimeters the value from the sensor
+      if(WiFi.status()== WL_CONNECTED){
+        if(distance_reading!= "0.00") {
+          WiFiClient client;
+          HTTPClient http;
+          http.begin(client, serverName);
+          http.addHeader("Content-Type", "application/json");
+          String jsonString = "{\"level\":\"" + distance_reading + "\"}";
+          int httpResponseCode = http.POST(jsonString);
+          Serial.print("HTTP Response code: ");
+          Serial.println(httpResponseCode);
+          Serial.print(distance_reading);
+          // Free resources
+          http.end();
+          //delaytime = 10000;
+          if (httpResponseCode == 200) {
+            clockUpdateCounter = 0;
+          }
+          else {
+            clockUpdateCounter = 1439;
+          }
+        }
+        else {
+          clockUpdateCounter = 1439;
+        }
+      }
+    }
+  }
+  clockUpdateCounter++;
+  delay(delaytime);
 }
